ac897144b87d228302005c12391dfd36c9291f21
eduardomausa <mausaeduardo@gmail.com>
2022-07-26 19:54:13 -0300

    API Routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import { AppProps } from "../../node_modules/next/app"
+import { AppProps } from '../../node_modules/next/app'
+import { Header } from '../components/Header/index';
+import { SessionProvider } from "next-auth/react"
+
+
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp
